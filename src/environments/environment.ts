73a20a49e48eba3060cd21eb8b1a8a727fab73b1
USER <mireya.garcia@epn.edu.ec>
2025-05-08 21:44:08 -0500

    Configuracìon inicial, proyecto con creedenciales del ingeniero en cargado.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false, 
   firebaseConfig: {
-    apiKey: "AIzaSyAap9r82buIPdTT_l7ppgm3L94j2POZeIM",
-    authDomain: "formulario-2b1c0.firebaseapp.com",
-    projectId: "formulario-2b1c0",
-    storageBucket: "formulario-2b1c0.firebasestorage.app",
-    messagingSenderId: "951322460453",
-    appId: "1:951322460453:web:081a7042d37f4e8d56dacc",
-    measurementId: "G-43PL12YJ5Q"
+    apiKey: "AIzaSyByQgiCUzf3gYfFYcw4Pl7Rec-yRFjSIZA",
+    authDomain: "a-e2689.firebaseapp.com",
+    projectId: "a-e2689",
+    storageBucket: "a-e2689.firebasestorage.app",
+    messagingSenderId: "941773529283",
+    appId: "1:941773529283:web:8a9199b937e0fe88a88276",
+    measurementId: "G-PB8QCM0HKG"
   }
 };
 
